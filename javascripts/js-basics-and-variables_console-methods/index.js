da12656ccf1f383e4309f7ae4c283e6e3a59ff63
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2025-12-11 16:05:52 +0100

    alle challenges done

--- javascripts/js-basics-and-variables_console-methods/index.js
+++ javascripts/js-basics-and-variables_console-methods/index.js
@@ -5,17 +5,23 @@ console.clear();
 // For reference, check out all console methods on MDN: https://developer.mozilla.org/en-US/docs/Web/API/console
 
 // 1. Store the sentence 'Happy coding!' in a variable named 'coding'.
+const coding = 'Happy code'
+console.log(coding)
 
 // 2. Display the variable 'coding' as a message in the console.
 
-// 3. Store the sentence 'Happy hacking!' in a variable named 'hacking'.
 
+// 3. Store the sentence 'Happy hacking!' in a variable named 'hacking'.
+const hacking = "Happy coding"
 // 4. Display the variable 'hacking' as a message in the console.
+console.log(hacking)
 
 // 5. Which method is used to clear the console? Use it to tidy up the console.
+console.clear()
 
 // 6. Store the text 'This is an error!' in a variable 'error'.
+const erorre = "this is an erorre"
 
 // 7. Which method is used to display an error message in the console? Use it do display the error message in the console.
-
+console.error(erorre)
 // Well done!
