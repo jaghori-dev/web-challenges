45f360917d00baf1334c21c0fd91e535fb25a2d0
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2025-12-18 15:42:01 +0100

    form submit done

--- javascripts/js-forms/js-forms_handle-form-submit/js/index.js
+++ javascripts/js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+        // console.log(event.target)
+        // console.log(event.target.elements.firstName.value);
+        // console.log(event.target.elements.lastName.value);
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+        event.target.reset();
+        event.target.elements.firstName.focus();
+        // const name = event.target.elements.firstName.value
+        // const ageP = event.target.elements.age.value
+        // const howBad = event.target.elements.badness.value
+        // console.log(name ,"is " ,ageP + howBad)
+
+})
