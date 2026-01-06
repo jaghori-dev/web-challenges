76b3166d9c3348ef952b336257709c50d2e80be7
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-06 12:05:49 +0100

    add js-loop for-of-list

---
+++ javascripts/js-loops/js-loops_forof-list/js/index.js
@@ -0,0 +1,24 @@
+console.clear();
+
+const main = document.querySelector('[data-js="main"]');
+
+const ol = document.createElement("ol");
+main.append(ol);
+const programmingLanguages = [
+  "JavaScript",
+  "Python",
+  "Java",
+  "C#",
+  "C++",
+  "PHP",
+  "Ruby",
+];
+
+// --v-- write or modify code below this line --v--
+for (let i of programmingLanguages){
+  let li = document.createElement("li")
+  li.textContent = i
+  ol.append(li)
+}
+
+// --^-- write or modify code above this line --^--
