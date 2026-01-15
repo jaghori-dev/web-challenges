e0171d9cbbf6acc1c01e9ea217b932e6d35ea826
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-15 16:24:28 +0100

    react basics challenge done

--- React/react-basics_hello-world/src/App.jsx
+++ React/react-basics_hello-world/src/App.jsx
@@ -1,5 +1,14 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return (
+    <div>
+      <h1>first react day</h1>
+      <p>hello world</p>
+      <p>hello again</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore hic repellat perspiciatis ullam quis exercitationem eos eligendi facilis aliquid porro.</p>
+    </div>
+  )
+}
+export function HelloWorld(){
 }
