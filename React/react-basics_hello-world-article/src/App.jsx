c3df42b38e3d146a1f9790eb860dcefee150d4a3
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-15 16:33:53 +0100

    react challenge 2 done

---
+++ React/react-basics_hello-world-article/src/App.jsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function HeeloWorldArtical() {
+  return (
+    <article>
+      <h1>Heading</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad quisquam est, repellat veniam cupiditate cumque obcaecati laudantium sapiente doloribus voluptatibus? Explicabo natus error et nesciunt pariatur dolorem, ad optio eaque?
+      </p>
+      <div>
+        <h2>Heading two</h2>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
+    </article>
+  )
+}
