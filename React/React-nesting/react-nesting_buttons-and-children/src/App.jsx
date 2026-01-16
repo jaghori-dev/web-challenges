1dc4f9a7781d61f82091993ec693e33d24f0b238
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-16 17:10:38 +0100

    react neasting challenge 1 done

--- React/React-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ React/React-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -2,20 +2,36 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function click(text, emoji) {
+    console.log("clicked", text, emoji);
+  }
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      {/* <Button text="next" emoji="👏" callback={click} />
+      <Button text="previous" emoji="👀" callback={click}/>
+      <Button text="up" emoji="☠️" callback={click}/>
+      <Button text="down" callback={click} emoji="👺"/>
+       */}
+      <button>button 1</button>
+      <button>button 2</button>
+      <button>button 3</button>
+      <button>button 4</button>
     </main>
   );
 }
 
-function Button() {
+// function Button({text, emoji, callback}) {
+//   return (
+//     <button className="button" type="button" onClick={()=>callback(text, emoji)}>
+//       {text}
+//     </button>
+//   );
+// }
+
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
