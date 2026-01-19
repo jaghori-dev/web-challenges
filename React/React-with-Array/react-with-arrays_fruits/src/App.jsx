e838cb98043441bb47ac9102b125b5e6c1ad2101
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-19 17:14:41 +0100

    react with array fruits challenge done

--- React/React-with-Array/react-with-arrays_fruits/src/App.jsx
+++ React/React-with-Array/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,51 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { 
+      id: 0,
+      name: "Apple",
+      color: "red",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis natus eum numquam recusandae adipisci exercitationem esse reprehenderit praesentium aliquam hic."
+    },
+    {
+      id: 1,
+      name: "Orange",
+      color: "orange",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis natus eum numquam recusandae adipisci exercitationem esse reprehenderit praesentium aliquam hic."
+
+    },
+    {
+      id: 2,
+      name: "Banana",
+      color: "yellow",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis natus eum numquam recusandae adipisci exercitationem esse reprehenderit praesentium aliquam hic."
+    },
+    {
+      id: 3,
+      name: "kiwi",
+      color: "gray",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis natus eum numquam recusandae adipisci exercitationem esse reprehenderit praesentium aliquam hic."
+    },
+    {
+      id: 4,
+      name: "grap",
+      color: "gold",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis natus eum numquam recusandae adipisci exercitationem esse reprehenderit praesentium aliquam hic."
+    },
+  ];
 
   return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
+    <main className="app">
+      <ul>
+        {fruits.map((fruit) => {
+          return (
+            <li key={fruit.id}>
+              <Card fruit={fruit} />
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
 }
--- React/React-with-Array/react-with-arrays_fruits/src/components/Card.jsx
+++ React/React-with-Array/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,13 @@
+import { Fragment } from "react/jsx-runtime";
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ fruit}) {
+  const {name, color, description} = fruit
+  return (
+    <section className={color === "red"? "card card--red":"" || color === "yellow"? " card card--yellow": "card"} >
+      <h1>{name}</h1>
+      <p>{color}</p>
+      <p>{description}</p>
+    </section>
+  );
 }
