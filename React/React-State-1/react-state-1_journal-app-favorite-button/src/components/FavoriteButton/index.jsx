5cb66a3fda821f82f14f7a3193b15a89ad612f26
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-21 12:44:13 +0100

    react state-1 journal app favorite button challenge done

--- React/React-State-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
+++ React/React-State-1/react-state-1_journal-app-favorite-button/src/components/FavoriteButton/index.jsx
@@ -1,17 +1,18 @@
 import "./FavoriteButton.css";
 import StarFilled from "./star-filled.svg?react";
 import Star from "./star.svg?react";
+import { useState } from "react";
 
 export default function FavoriteButton() {
-  // This should be a state variable.
-  const isFavorite = false;
+  const [isFavorite, setisFavorite] = useState(false);
+  function handleFavorite() {
+    setisFavorite(prev => !prev);
+  }
 
   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        console.log("favorite button clicked");
-      }}
+      onClick={handleFavorite}
       aria-label="favorite"
     >
       {isFavorite ? <StarFilled /> : <Star />}
