d4d20924c97618936d6d982f3ab3fe32380f4718
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-21 10:52:22 +0100

    react props button challenge done

--- React/React-props/react-props_buttons/src/App.jsx
+++ React/React-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,16 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <>
+  <Button text="click me" color= "green" disable={false}/>
+  <Button text="Submit" color= "blue"/>
+  <Button text="danger" color= "red" disable={true}/>
+  </>;
+}
+
+const Button = ({color, text, disable})=>{
+  const handleClick = (text)=>{
+    console.log("you clicked ", text)
+  }
+  return (
+    <button onClick={()=> handleClick(text)} disabled={disable} style={{backgroundColor: color, width: '90px', height:'40px', margin: '10px'}}>{text}</button>
+  )
 }
