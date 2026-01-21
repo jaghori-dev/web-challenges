39b8977cb7f4140d86128cd050c822b53f11dc99
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-01-21 17:21:56 +0100

    react state-3 tags challenge done

--- React/React-state-3/react-state-3_tags/src/App.jsx
+++ React/React-state-3/react-state-3_tags/src/App.jsx
@@ -5,11 +5,19 @@ import List from "./components/List";
 
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
-
+  function addTag(data){
+    console.log(data)
+    setTags([...tags , data])
+  }
+  function onDeleteTag(input){
+    const newTags = tags.filter((tag)=> tag !== input)
+    console.log(newTags)
+    setTags(newTags)
+  }
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={addTag} />
+      <List tags={tags} onDeleteTag={onDeleteTag}/>
     </main>
   );
 }
