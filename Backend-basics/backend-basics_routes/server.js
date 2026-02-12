7350d096b9ea23f194058cbf8fd5406b734a8218
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-12 16:13:43 +0100

    backend basics routes challenge done:

--- Backend-basics/backend-basics_routes/server.js
+++ Backend-basics/backend-basics_routes/server.js
@@ -1,3 +1,19 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response)=>{
+    console.log(request.url)
+    if (request.url === '/api/fish/1'){
+        response.end('Shrimp')
+        return
+    }
+    if(request.url === '/api/fish/2'){
+        response.statusCode = 200
+        return response.end('Shark')
+    }
+    response.statusCode = 404
+    response.end("not found")
+})
+
+// server.listen(3000, ()=> {
+//     console.log("listening on http://localhost:3000")
+// })
