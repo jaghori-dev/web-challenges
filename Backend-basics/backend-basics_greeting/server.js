a314ab4f1269ff2be4ee0810ffb420e8702c2dae
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-12 15:48:10 +0100

    backend basics greeting challenge done

--- Backend-basics/backend-basics_greeting/server.js
+++ Backend-basics/backend-basics_greeting/server.js
@@ -1,3 +1,10 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  console.log(request.url);
+    response.end('Hello world!!!')
+});
+
+server.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
