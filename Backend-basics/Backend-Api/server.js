7ea74a41e347cc158d15f3a6ce6a26fb26b7e1b8
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-12 17:18:30 +0100

    add backend api

---
+++ Backend-basics/Backend-Api/server.js
@@ -0,0 +1,27 @@
+import { createServer } from "node:http";
+import { json } from "node:stream/consumers";
+
+const server = createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/api") {
+    const details = [
+      {
+        name: "Ali",
+        age: 23,
+        country: "Germany",
+      },
+      {
+        name: "Ali",
+        age: 23,
+        country: "Germany",
+      },
+    ];
+    // res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(details));
+    return;
+  }
+});
+
+server.listen(8000, () => {
+  console.log("litening on http://localhost:8000");
+});
