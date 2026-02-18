8a581e06e9a237002a9916be22eb35f40f73b9f8
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-02-18 23:04:53 +0100

    add styles to card

--- Backend/backend-update-and-delete-movies/movies/pages/[id].js
+++ Backend/backend-update-and-delete-movies/movies/pages/[id].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -13,15 +14,37 @@ export default function details() {
     error,
   } = useSWR(`/api/movies/${id}`, fetcher);
   if (isLoading) return <h1>loading...</h1>;
-  if(!movie) return <p>somthing went wrong </p>
+  if (!movie) return <h1>Movie not found</h1>;
   console.log(movie);
   return (
-    <div className="container">
-      <h1>{movie.title}</h1>
-      <Image src={movie?.poster} alt={movie.title} width={200} height={260} />
-      <p>{movie.directors}</p>
-      <p>{movie.year}</p>
-      <p>{movie.plot}</p>
+    <div className={styles.main}>
+      <div className={styles.card}>
+        <h1>{movie.title}</h1>
+        <Image src={movie?.poster} alt={movie.title} width={200} height={260} />
+        <br />
+        <p>{movie.year}</p>
+      </div>
+      <div className={styles.container}>
+        <ul className={styles.card}>
+          <h4>Directors</h4>
+          {movie.directors?.map((name) => {
+            return <li key={name}>{name}</li>;
+          })}
+        </ul>
+        <ul className={styles.card}>
+          <h4>Genres</h4>
+          {movie.genres?.map((name) => {
+            return <li key={name}>{name}</li>;
+          })}
+        </ul>
+        <ul className={styles.card}>
+          <h4>Writers</h4>
+          {movie.writers?.map((name) => {
+            return <li key={name}>{name}</li>;
+          })}
+        </ul>
+        <p className={styles.card}>{movie.plot}</p>
+      </div>
     </div>
   );
 }
--- Backend/backend-update-and-delete-movies/movies/pages/index.js
+++ Backend/backend-update-and-delete-movies/movies/pages/index.js
@@ -1,6 +1,8 @@
 import useSWR from "swr";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Head from "next/head";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -11,24 +13,29 @@ export default function Home() {
   if (!movies || movies.length === 0) return <h1>movie not found</h1>;
   console.log("movies", movies);
   return (
-    <main className="main">
-      <h1 className="title">Movies</h1>
-      <ul className="container">
-        {movies.map((movie) => (
-          <li key={movie._id} className="container">
-            <Link href={`/${movie._id}`}>
-              <h2 className="title">{movie.title}</h2>
-            </Link>
-            <Image
-              src={movie?.poster}
-              alt={movie.title}
-              width={200}
-              height={260}
-            />
-            <p>{movie.year}</p>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <div className={styles.container}>
+      <Head>
+        <title>Movies</title>
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Movies</h1>
+        <ul className={styles.grid}>
+          {movies.map((movie) => (
+            <li key={movie._id} className={styles.card}>
+              <Link href={`/${movie._id}`}>
+                <h2 className={styles.title}>{movie.title}</h2>
+              </Link>
+              <Image
+                src={movie.poster}
+                alt={movie.title}
+                width={260}
+                height={300}
+              />
+              <p>{movie.year}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
